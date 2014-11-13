edd0f0392cef7be78244c8663fc12a1e05d16866
Milan Herbig <milanherbig@gmail.com>
2014-11-13 18:45:48 +0100

    LogCleaner refactor

--- components/scheduler.py
+++ components/scheduler.py
@@ -22,36 +22,36 @@ class LogCleaner(QObject):
     This behaviour leeds to high amount of redundant (nothing saying) log files.
     Log file is important only when error occurs and user want to participate and provide logs.
     Log cleaner runs in separated thread.
+    Worker method: LogCleaner.clean()
     """
 
     def __init__(self):
         super(LogCleaner, self).__init__()
 
-        self.LOG_DIR = os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), 'log')
-        self.DELAY = 5 * (60 * 1000)                        # 5 minutes in ms
+        self.log_dir = os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), 'log')
+        self.delay = 5 * (60 * 1000)                        # 5 minutes in ms
 
         logger.debug(u"LogCleaner initialized")
 
     def start(self):
-        logger.debug(u"Scheduling log cleaner - delay %s", self.DELAY)
-        QTimer.singleShot(self.DELAY, self.clean)
+        logger.debug(u"Scheduling log cleaner - delay %s", self.delay)
+        QTimer.singleShot(self.delay, self.clean)
 
     def clean(self):
         """
-        Thread worker.
         Deletes old files from /log folder.
-        :return:
+        Thread worker.
         """
-        if not os.path.isdir(self.LOG_DIR):
-            logger.error(u"Log dir under '%s' doesn't exist!", self.LOG_DIR)
+        if not os.path.isdir(self.log_dir):
+            logger.error(u"Log dir under '%s' doesn't exist!", self.log_dir)
 
         logger.debug(u"Launching scheduled log file cleaning...")
 
         now = time.time()
         one_day = 86400
         time_past_limit = now - (7 * one_day)
-        for ffile in os.listdir(self.LOG_DIR):
-            fpath = unicode(os.path.join(self.LOG_DIR, ffile), sys.getfilesystemencoding())
+        for ffile in os.listdir(self.log_dir):
+            fpath = unicode(os.path.join(self.log_dir, ffile), sys.getfilesystemencoding())
 
             # if file is older than X days, than remove the file
             if os.stat(fpath).st_mtime < time_past_limit:
